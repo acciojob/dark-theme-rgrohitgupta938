8a15d490a6e797954c10be320828ff6850d75d1c
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 02:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//your code here
-function swapTheme(){
-	const app = document.getElementById("app");
-	app.classList.add("night");
+function swapTheme() {
+  const app = document.getElementById("app");
+  const btn = document.getElementById("swap");
+  if (app.classList.contains("day")) {
+    app.classList.remove("day");
+    app.classList.add("night");
+    btn.classList.remove("button-day");
+    btn.classList.add("button_night");
+  } else {
+    app.classList.remove("night");
+    app.classList.add("day");
+    btn.classList.remove("button_night");
+    btn.classList.add("button-day");
+  }
 }
